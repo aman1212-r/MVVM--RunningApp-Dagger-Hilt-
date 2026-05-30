e9cd567ed3fe12390206026cdbee8f395bdda01a
aman1212-r <aman1212rana@gmail.com>
2026-05-30 11:15:04 +0530

    Minnor Changes

--- app/src/main/java/com/example/runningapp/MainActivity.kt
+++ app/src/main/java/com/example/runningapp/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.runningapp.ui.theme.RunningAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +18,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RunningAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize())
+                { padding ->
+                    Text(
+                        text = "Hello World!",
+                        modifier = Modifier.padding(padding)
                     )
                 }
             }
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RunningAppTheme {
-        Greeting("Android")
-    }
-}
